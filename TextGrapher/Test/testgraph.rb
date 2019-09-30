a1fc562c0e8e28a899b0048520c65be7b73da89c
Packmanager9 <36295363+Packmanager9@users.noreply.github.com>
2019-09-30 08:46:18 -0700

    commenting
    Creating documentation

--- TextGrapher/Test/testgraph.rb
+++ TextGrapher/Test/testgraph.rb
@@ -1,10 +1,10 @@
-require "TextGrapher"
-graph = Input_set.new([], 1400, 100, 50, 1, 1, 0,  0, "testgraphfile")
+require "TextGrapher" ## include this line to construct graphs in any file
+graph = Input_set.new([], 720, 100, 50, 1, 1, 0,  0, "testgraphfile")  #   positions, xrange, yrange, trackline_height = (xrange/2).to_i, xstep = 1, ystep = 1, xstart = 0, ystart = 0, filename ="graph", slopemode = false.   Everything but the range and position array are optional, but casn be used to further define thr graph
 
-graph.graphit("testoutput") do |xdim|
-    xdim =      (xdim * 0.0174533) #                      to convert radians to degrees (xdim*0.0174533)
-    xdim =  (50+(30*Math.cos(Math.sin(xdim)))).to_i
-    xdim.to_i
-    xdim
+graph.graphit("testoutput") do |xdim|  ## graphit takes a block to define the curve its roughly of the form Y = |block return|   The "testoutput" parameter for graphit tells the program to name the output file testoutput.txt
+    xdim =      (xdim * 0.0174533) #  to convert radians to degrees (xdim*0.0174533) for trig graphs, Math module defaults to radians,. 
+    xdim =  (50+(30*Math.cos(Math.sin(xdim)))).to_i   ## this part can be anything you need, it is currently a trig wave for the example output
+    xdim.to_i  ## always include a ".to_i" for the graph to print correctly. To print values between whole numbers multiply the curve and chart as if the multiple was 1 
+    xdim  ## block must return the value being manipulated implicitly as explicit return will cause the program to end early
 end
 
